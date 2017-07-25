b222302b772882809d9851cadd39ad85e45eb2bc
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-25 17:46:16 -0400

    add essential components

--- src/main/java/com/zuolizhu/todoapp/TodoUI.java
+++ src/main/java/com/zuolizhu/todoapp/TodoUI.java
@@ -1,14 +1,49 @@
 package com.zuolizhu.todoapp;
 
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.UI;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Bugu on 7/25/2017.
  */
+
+@SpringUI
 public class TodoUI extends UI {
+
+    private VerticalLayout root;
+
+    @Autowired
+    Todolayout todolayout;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        setupLayout();
+        addHeader();
+        addForm();
+        addTodoList();
+        addDeleteButton();
+    }
 
+    private void setupLayout() {
+        root = new VerticalLayout();
+        setContent(root);
+    }
+    private void addHeader() {
+        root.addComponent(new Label("Add your TODOs"));
+    }
+    private void addForm() {
+        HorizontalLayout formlayout = new HorizontalLayout();
+        TextField task = new TextField();
+        Button add = new Button("Add");
+        formlayout.addComponents(task, add);
+        root.addComponent(formlayout);
+    }
+    private void addTodoList() {
+        root.addComponent(todolayout);
+    }
+    private void addDeleteButton() {
+        root.addComponent(new Button("Delete completed"));
     }
 }
---
+++ src/main/java/com/zuolizhu/todoapp/Todolayout.java
@@ -0,0 +1,11 @@
+package com.zuolizhu.todoapp;
+
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Created by Bugu on 7/25/2017.
+ */
+@SpringComponent
+public class Todolayout extends VerticalLayout {
+}
