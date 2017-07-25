e187534c2d9a3fc5061f2d37b9fa6380c49aac97
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-25 17:31:15 -0400

    vaadin framework firsttry

---
+++ src/main/java/com/zuolizhu/todoapp/TodoUI.java
@@ -0,0 +1,14 @@
+package com.zuolizhu.todoapp;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * Created by Bugu on 7/25/2017.
+ */
+public class TodoUI extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
---
+++ src/main/java/com/zuolizhu/todoapp/TodoappApplication.java
@@ -0,0 +1,12 @@
+package com.zuolizhu.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+}
