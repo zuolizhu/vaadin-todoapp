e603075f127dda6fe5874985f93333d580de7880
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-25 18:00:39 -0400

    add plus icon

--- src/main/java/com/zuolizhu/todoapp/TodoUI.java
+++ src/main/java/com/zuolizhu/todoapp/TodoUI.java
@@ -1,5 +1,6 @@
 package com.zuolizhu.todoapp;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
@@ -41,9 +42,9 @@ public class TodoUI extends UI {
         HorizontalLayout formLayout = new HorizontalLayout();
         formLayout.setWidth("80%");
         TextField task = new TextField();
-        Button add = new Button("Add");
+        Button add = new Button("");
         add.addStyleName(ValoTheme.BUTTON_PRIMARY);
-
+        add.setIcon(VaadinIcons.PLUS);
         formLayout.addComponentsAndExpand(task);
         formLayout.addComponents(add);
         root.addComponent(formLayout);
