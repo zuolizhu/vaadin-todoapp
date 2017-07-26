489c016c7c8cac4ccb0aef786f226680995f5d0d
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-25 23:24:26 -0400

    database setup

--- src/main/java/com/zuolizhu/todoapp/Todolayout.java
+++ src/main/java/com/zuolizhu/todoapp/Todolayout.java
@@ -2,10 +2,25 @@ package com.zuolizhu.todoapp;
 
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Created by Bugu on 7/25/2017.
  */
 @SpringComponent
 public class Todolayout extends VerticalLayout {
+    @Autowired
+    TodoBucket bucket;
+
+    @PostConstruct
+    void init() {
+        setTodos(bucket.findAll());
+    }
+
+    private void setTodos(List<Todo> todos) {
+        removeAllComponents();
+    }
 }
