90d24eda118c9b63521167d557bc43093ed9672e
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-26 12:45:34 -0400

    finished

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS Todo(id IDENTITY PRIMARY KEY, done BOOLEAN, text VARC
 DELETE FROM Todo;
 INSERT INTO Todo VALUES(1, true, 'Prepare presentation');
 INSERT INTO Todo VALUES(2, true, 'Procrastinate');
-INSERT INTO Todo VALUES(3, false, 'Have presentation');
+INSERT INTO Todo VALUES(3, true, 'Have presentation');
